f250060b6d3c54837266bea97ea00a486f98b711
fustte.olivergui <fustte.olivergui@gmail.com>
2025-04-03 12:01:30 +0000

    logica para receber o valor da porcetagem e calculosndos valores da porcentagem por pessoa e total por pessoa

--- main.js
+++ main.js
@@ -1,63 +1,103 @@
+
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
 let buttonSelected = null
 
-let billInput = document.querySelector("#bill")
-billInput.addEventListener("input" , receiveBillValue )
-
-let numberOfPeopleInput = document.querySelector("#people")
-numberOfPeopleInput.addEventListener("input" , receiveNumberOfPeopleValue )
-
 function receiveBillValue() {
-   bill = billInput.valueAsNumber
-   
-   calculate()
+    bill = document.querySelector("#bill").valueAsNumber
+    calculate()
+
+    
 }
 
 function receiveNumberOfPeopleValue() {
-    numberOfPeople = numberOfPeopleInput.valueAsNumber
+     numberOfPeople = document.querySelector("#people").valueAsNumber
+     calculate()
 
-    console.log (numberOfPeople)
+
 }
 
+function receiveTipPercentageValue(value) {
+    removeButtonSelectedClass()
 
-function receiveTipPercentageValue(value){
+    let customTipInput = document.querySelector("#custom-tip")
 
+    
+   if (customTipInput.value !== "") {
+        customTipInput.value = ""
+
+    }
+    
     tipPercentage = value / 100
-
-    removeClassButtonSelected()
-
     
-    
-   
+
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+    calculate()
+
 
 }
 
+function receiveCustomTipPercentageValue() {
+    removeButtonSelectedClass()
+    buttonSelected = null
+ 
+      tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+      calculate()
+      
 
-function receiveCustomTipPercentageValue(){
-    tipPercentage = document.queryySelector("#custom-tip").valueAsNumber / 100
 
-   removeClassButtonSelected()
-}
-
-function removeClassButtonSelected(){
-    if(buttonSelected !== null){
-        buttonSelected.classList.remove("button-selected")
-        buttonSelected = null
-    }
 }
 
 function calculate() {
+      if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+           let amountStrong = document.querySelector(".amount strong")
+           let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
 
-   if(bill !==0 && tipPercentage !==0 && numberOfPeople !== 0) {
-    console.log("é possivel calcular")
-   } else {
-    console.log("Ainda não dá!  Preencha tudo")
-   }
+           amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
 
+           let totalStrong = document.querySelector(".total strong")
+           let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+           totalStrong.innerText = `$${totalPerson.toFixed(2)}`
 
+      }  else {
+           console.log("preencha tudo")
+
+      }
 
 }
+
+function reset() {
+    bill = 0
+    document.querySelector("#bill").value = ""
+     
+    
+    numberOfPeople = 0
+    document.querySelector("#people").value = ""
+
+    tipPercentage = 0
+    removeButtonSelectedClass()
+
+    let customTipInput = document.querySelector("#custom-tip")
+
+    
+   if (customTipInput.value !== "") {
+        customTipInput.value = ""
+
+    }
+
+    document.querySelector(".amount strong").innerText = "$0.00"
+    document.querySelector(".total strong").innerText = "$0.00"
+
+}
+
+function removeButtonSelectedClass() {
+
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+    }
+
+}
+
+
